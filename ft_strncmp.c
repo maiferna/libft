780cf7bea58bb0e4e9ebf3ab2c164ea21714a2ad
maiferna <maiderfernandez57@gmail.com>
2024-09-30 13:28:30 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -29,7 +29,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	{
 		if (s1[i] != '\0' || s2[i] != '\0')
 		{
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 	}
 	return (0);
