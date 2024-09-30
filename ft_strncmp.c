23a64017c7fbd22f5df6cc722bf614b6e859cf8b
maiferna <maiderfernandez57@gmail.com>
2024-09-30 14:40:41 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -17,20 +17,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && s1[i] != '\0' && s2[i] != '\0')
+	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
 		i++;
 	}
-	if (i < n)
-	{
-		if (s1[i] != '\0' || s2[i] != '\0')
-		{
-			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-		}
-	}
-	return (0);
+	if (i == n)
+		return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
